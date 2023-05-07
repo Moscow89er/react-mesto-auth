c83066fa19615c90197e38c1738e1c907470b536
Moscow89er <116744782+Moscow89er@users.noreply.github.com>
2023-05-07 18:23:48 +0400

    Delete mesto-react-auth directory

--- mesto-react-auth/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p className="footer__copyright">© 2022 Никита Хубаев</p>
-        </footer>
-    );
-}
-
-export default Footer;
